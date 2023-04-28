545e1aa77bc919ca9ca4b86c14831dfd1725c5d1
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-04-28 20:29:41 +0200

    protecion

--- ft_otp.py
+++ ft_otp.py
@@ -6,7 +6,7 @@
 #    By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/20 18:01:34 by tvillare          #+#    #+#              #
-#    Updated: 2023/04/28 17:14:05 by tvillare         ###   ########.fr        #
+#    Updated: 2023/04/28 20:09:59 by tvillare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,19 +19,19 @@ import hmac
 import base64
 import struct
 import time
-from cryptography.fernet import Fernet
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
 
 parser = argparse.ArgumentParser(description='')
 
 parser.add_argument('-g',
 					type=str,
-					#action='store_true',
 					dest='g',
 					help="pide fichero hexadecimal y debuelbe .key")
 
 parser.add_argument('-k',
 					type=str,
-					#action='store_true',
 					dest='k',
 					help="pide un .key y retorna una clave de tiempo")
 
@@ -39,28 +39,34 @@ args = parser.parse_args()
 
 re_hexa = re.compile("^[0-9a-fA-F]+$")
 
+# Definir clave maestra
+clave_maestra = b"clave_maestra_1234"
+clave_maestra = clave_maestra[:16]
 
 
+def cifrar(mensaje, clave_maestra):
+	# Generar un vector de inicialización aleatorio
+	iv = get_random_bytes(16)
 
-def cifrado_fernet(message):
-	# Generar una clave secreta aleatoria
-	key = Fernet.generate_key()
+	# Crear un cifrador AES con la clave maestra y el vector de inicialización
+	cifrador = AES.new(clave_maestra, AES.MODE_CFB, iv)
 
-	# Crear un objeto Fernet con la clave secreta
-	cipher_suite = Fernet(key)
+	# Cifrar el mensaje y agregar el vector de inicialización al inicio del mensaje cifrado
+	mensaje_cifrado = iv + cifrador.encrypt(mensaje.encode())
 
-	# Cifrar un mensaje
-	cipher_text = cipher_suite.encrypt(message.encode())
-	return cipher_text
+	return mensaje_cifrado
 
+def descifrar(mensaje_cifrado, clave_maestra):
+	# Obtener el vector de inicialización del mensaje cifrado
+	iv = mensaje_cifrado[:16]
 
+	# Crear un descifrador AES con la clave maestra y el vector de inicialización
+	descifrador = AES.new(clave_maestra, AES.MODE_CFB, iv)
 
-def decoder_fernet(message):
-	key = Fernet.generate_key()
-	cipher_suite = Fernet(key)
-	plain_text = cipher_suite.decrypt(message)
-	return plain_text
+	# Descifrar el mensaje
+	mensaje_descifrado = descifrador.decrypt(mensaje_cifrado[16:]).decode()
 
+	return mensaje_descifrado
 
 
 ####crear id de tiempo
@@ -78,20 +84,19 @@ def get_hotp_token(secret, intervals_no):
 
 def get_totp_token(secret):
 	#ensuring to give the same otp for 30 seconds
-	x =str(get_hotp_token(secret,intervals_no=int(time.time())//30))
+	x = str(get_hotp_token(secret,intervals_no=int(time.time())//30))
 	#adding 0 in the beginning till OTP has 6 digits
 	return x.zfill(6)
+
 ######crear .key
 
-
-
-def leer_fichero(file):
-	if os.access(file, os.R_OK):
-		archivo = open(file, "r")
+def leer_fichero(file, mode):
+	try:
+		archivo = open(file, mode)
 		# Lee el contenido del archivo usando read()
 		contenido = archivo.read()
 		archivo.close()
-	else:
+	except:
 		print("Error file")
 		sys.exit()
 	return contenido
@@ -102,25 +107,25 @@ def create_file_key(file, contenido):
 	with open(name, "wb") as archivo:
 		archivo.write(contenido)
 		archivo.close()
+	print(f"Key was successfully saved in {name}.")
 
 def create_key(file):
-	hexa = leer_fichero(file)
-	if (len(hexa) < 64) and (re_hexa.match(hexa)):
+	hexa = leer_fichero(file, "r")
+	if not(re_hexa.match(hexa)) or len(hexa) < 64:
 		print("./ft_otp: error: key must be 64 hexadecimal characters.")
 		return
-	#hash_object = hashlib.sha1(hexa.encode())
-	# Convertimos el valor hash a una cadena hexadecimal
-	#hex_dig = hash_object.hexdigest()
-	hex_dig = cifrado_fernet(hexa)
+	hex_dig = cifrar(hexa, clave_maestra)
 	create_file_key(file, hex_dig)
-
-
 
 
 if (args.g != None):
 	create_key(args.g)
 elif (args.k != None):
-	secret = leer_fichero(args.k)
-	plain_text = decoder_fernet(secret)
-	key = get_totp_token(secret)
-	print(key)
+	secret = leer_fichero(args.k, "rb")
+	try:
+		plain_text = descifrar(secret, clave_maestra)
+	except:
+		print("Error descifrado")
+	else:
+		key = get_totp_token(plain_text)
+		print(key)
